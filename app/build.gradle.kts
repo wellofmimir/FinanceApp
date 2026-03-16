da68f13f3d173ea6945ed315e8e9e007ae25bb77
Patryk Mleczko <mleczko.patryk.roman@gmail.com>
2026-03-17 00:22:38 +0100

    Version 1.48

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "studio.lemniscate.greeen"
         minSdk = 26
         targetSdk = 36
-        versionCode = 47
-        versionName = "1.47"
+        versionCode = 48
+        versionName = "1.48"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
